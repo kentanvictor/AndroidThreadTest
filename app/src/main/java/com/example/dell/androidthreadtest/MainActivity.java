862400ca40700b82d75ea2b1808d794738f51515
kentanvictor <2030457926@qq.com>
2017-06-07 16:50:33 +0800

    異步進行子線程的UI操作

--- app/src/main/java/com/example/dell/androidthreadtest/MainActivity.java
+++ app/src/main/java/com/example/dell/androidthreadtest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.dell.androidthreadtest;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +9,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-private TextView text;
+    public static final int UPDATE_TEXT = 1;
+    private TextView text;
+
+    private Handler handler = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+            switch (msg.what)
+            {
+                case UPDATE_TEXT:
+                    //在這裡可以進行UI操作
+                    text.setText("Nice to meet you");
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +38,19 @@ private TextView text;
 
     @Override
     public void onClick(View v) {
+        //在这里创建了一个子线程
         switch (v.getId())
         {
             case R.id.change_text:
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                      text.setText("Nice to meet you");
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);//將Message對象發出去
+                      /*text.setText("Nice to meet you");
+                        //在這裡，將原本TextView裡面將要顯示的字符串變成了Nice to meet you
+                        //但是，這是在子線程進行的UI更新，所以會程序崩潰的*/
                     }
                 }).start();
                 break;
